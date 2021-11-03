7a73730da1d6aa1f8d016798a4ff9cb17112ed55
Ben <mrsmish@yahoo.com>
2021-11-04 00:31:55 +0800

    fix sources.list

--- share/master.sh
+++ share/master.sh
@@ -4,20 +4,14 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
 sudo tee /etc/apt/sources.list <<-'EOF'
 deb http://mirrors.aliyun.com/ubuntu/ xenial main
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial main
 
 deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main
 
 deb http://mirrors.aliyun.com/ubuntu/ xenial universe
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial universe
 deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
 
 deb http://mirrors.aliyun.com/ubuntu/ xenial-security main
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main
 deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security universe
 EOF
 
 sudo apt-get update
